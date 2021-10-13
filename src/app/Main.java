9d5eb69b0a33d89d24d05bdc3d09d944f9e20936
Felipe Costa Gomes <73756949+FelipeCosmes@users.noreply.github.com>
2021-10-13 20:43:28 -0300

    Alterando a main para cadastrar pessoas.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,65 +1,62 @@
 package app;
 
-import javax.swing.JOptionPane;
-
-import entidades.Categoria;
-import entidades.SubCategoria;
 import entidades.Despesa;
 import entidades.Pessoa;
+import entidades.Republica;
+
+import javax.swing.*;
 
 public class Main {
-	
-	static Pessoa pessoas[] = new Pessoa[0];
-	static Despesa despesas[] = new Despesa[0];
-	static Categoria categorias[] = new Categoria[0];
-	
-	public static void cadastroPessoa() {
-		String nome = JOptionPane.showInputDialog("Informe o nome da pessoa.");
-		String email = JOptionPane.showInputDialog("Informe o email da pessoa");
-		String strValor = JOptionPane.showInputDialog("Informe a renda da pessoa, em R$");
-		float valor = Float.parseFloat(strValor);
-		
-		//criar uma pessoa
-		Pessoa clone = new Pessoa(nome,email, valor);
+    public static void main(String[] args) {
 
-		//Armazenar pessoa em clone
-		// Copiar pessoas para vetor clone
-		Pessoa[] clonePessoas = new Pessoa[pessoas.length + 1];
-		for (int i=0; i<pessoas.length; i++)
-			clonePessoas[i] = pessoas[i];
-		// Armazenar clone em clonePessoas
-		clonePessoas[clonePessoas.length-1] = clone;
-		//Atualizar a referencia pessoas
-		pessoas = clonePessoas;
-
-	}
-	
-	
-	public static void main(String[] args) {
-
-		int opcao = 0;
-
-		do {
-			String menu = "Informe a opção desejada: \n"
-					+ "1 - Cadastrar pessoa \n"
-					+ "2 - Cadastrar despesa  \n"
-					+ "0 - Sair";
-
-			String strOpcao = JOptionPane.showInputDialog(menu);
-			opcao = Integer.parseInt(strOpcao);
-
-			switch (opcao) {
-			case 1: 
-				cadastroPessoa();
-				break;
+        Pessoa pessoa = new Pessoa();
+        Despesa despesa = new Despesa();
+        Republica republica = new Republica();
 
 
-			} 
-		}
-		while (opcao != 0);
+        int opcao = 0;
+        do {
+            String menu = "Informe a opcao desejada: \n"
+                    + "1 - Cadastrar pessoa \n"
+                    + "2 - Excluir pessoa \n"
+                    + "3 - Calcular Divida \n"
+                    + "4 - Cadastrar despesa \n"
+                    + "5 - Excluir despesa \n"
+                    + "0 - Sair";
 
-		return;
-	}
-	
+            String strOpcao = JOptionPane.showInputDialog(menu);
+            opcao = Integer.parseInt(strOpcao);
 
+            switch (opcao) {
+                case 1:
+                    republica.cadastroPessoa();
+                    break;
+
+//                case 2:
+//                    republica.cadastroDespesa();
+//                    break;
+
+                case 3:
+                    String divida = Float.toString(pessoa.divida());
+                    JOptionPane.showMessageDialog(null, divida);
+                    break;
+
+//                case 4:
+//                    republica.excluirPessoa();
+//                    break;
+//
+//                case 5:
+//                    republica.excluirDespesa();
+
+                case 0:
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "OpÃ§Ã£o InvÃ¡lida!");
+                    break;
+            }
+        } while (opcao != 0);
+
+
+    }
 }
