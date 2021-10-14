a1bf044674a3ea41f46778818526a0736263d2a7
Sidney Fernando <88115743+nando3d3@users.noreply.github.com>
2021-10-13 22:18:32 -0300

    Update Republica.java

--- src/entidades/Republica.java
+++ src/entidades/Republica.java
@@ -1,69 +1,80 @@
 package entidades;
 
 import java.util.List;
+
+import javax.swing.JOptionPane;
+
 import java.util.ArrayList;
 
 public class Republica {
-	
+
 	private List<Pessoa> listaPessoas;
 	private List<Despesa> listaDespesas;
 	private boolean divisaoProporcional;
-	
-	public Republica () {
+
+	public Republica() {
 		listaPessoas = new ArrayList<>();
 		listaDespesas = new ArrayList<>();
 		divisaoProporcional = false;
 	}
-	
-	public List<Pessoa> getListaPessoas () {
+
+	public List<Pessoa> getListaPessoas() {
 		return listaPessoas;
 	}
-	
-	public void cadastroPessoa () {
-        	String nome = JOptionPane.showInputDialog("Nome: ");
-        	String email = JOptionPane.showInputDialog("Email: ");
-        	String strRenda = JOptionPane.showInputDialog("Renda: ");
-        	float renda = Float.parseFloat(strRenda);
-        	listaPessoas.add(new Pessoa(nome, email, renda, this));
-    }
-	
-	public void excluirPessoa (Pessoa pessoa) {
-		listaPessoas.remove(pessoa);
+
+	public void cadastroPessoa() {
+		String nome = JOptionPane.showInputDialog("Nome: ");
+		String email = JOptionPane.showInputDialog("Email: ");
+		String strRenda = JOptionPane.showInputDialog("Renda: ");
+		float renda = Float.parseFloat(strRenda);
+		listaPessoas.add(new Pessoa(nome, email, renda, this));
 	}
-	
-	public List<Despesa> getListaDespesas () {
-		return listaDespesas;
-	}
-	
-	public void cadastroDespesa (String descricao, float valor, Categoria categoria) {
+
+	public void cadastroDespesa() {
+		String descricao = JOptionPane.showInputDialog("Descrição: ");
+		String strCat = JOptionPane.showInputDialog("Categoria: ");
+		Categoria categoria = new Categoria(strCat);
+		String strValor = JOptionPane.showInputDialog("Valor: ");
+		float valor = Float.parseFloat(strValor);
+		//String resp = JOptionPane.showInputDialog("Subcategoria: ");
+
 		listaDespesas.add(new Despesa(descricao, valor, categoria));
 	}
-	
-	public void excluirDespesa (Despesa despesa) {
+
+	public void excluirPessoa(Pessoa pessoa) {
+		listaPessoas.remove(pessoa);
+	}
+
+	public List<Despesa> getListaDespesas() {
+		return listaDespesas;
+	}
+
+	public void excluirDespesa(Despesa despesa) {
 		listaDespesas.remove(despesa);
 	}
-	
-	public boolean getDivisaoProporcional () {
+
+	public boolean getDivisaoProporcional() {
 		return divisaoProporcional;
 	}
-	
-	public void setDivisaoProporcional (boolean divisaoProporcional) {
+
+	public void setDivisaoProporcional(boolean divisaoProporcional) {
 		this.divisaoProporcional = divisaoProporcional;
 	}
-	
-	public float rendaTotal () {
+
+	public float rendaTotal() {
 		float total = 0;
-		for (Pessoa pessoa: listaPessoas) {
+		for (Pessoa pessoa : listaPessoas) {
 			total += pessoa.getRenda();
 		}
 		return total;
 	}
-	
-	public float despesaTotal () {
+
+	public float despesaTotal() {
 		float total = 0;
 		for (Despesa despesa : listaDespesas) {
 			total += despesa.getValor();
 		}
 		return total;
 	}
+
 }
