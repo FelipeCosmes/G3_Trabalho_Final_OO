e7dc84ecfcd796ab56aa04f7e1c69f47742ea811
douglas555 <31639885+douglas555@users.noreply.github.com>
2021-10-23 18:43:22 -0300

    Criando Exceções para cadastro de Despesas.

---
+++ src/exceptions/DescricaoNaoInformadaException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class DescricaoNaoInformadaException extends NullPointerException{
+
+
+	private static final long serialVersionUID = 1L;
+	
+
+}
---
+++ src/exceptions/ValorNaoInformadoException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class ValorNaoInformadoException extends NullPointerException{
+
+	
+	private static final long serialVersionUID = 1L;
+
+}
